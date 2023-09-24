99619e77e135c3ebbc09b39f5e29ccead4cc954f
sj00coder <jshubham94110@gmail.com>
2023-09-24 16:21:57 +0530

    Fix Loading Issue on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,14 @@
+import { useAuthState } from 'react-firebase-hooks/auth';
 import MainSection from '../components/homePage/MainSection';
 import SideNav from '../components/sideNav/SideNav';
+import Loader from '../components/Loader';
+import { auth } from '../Firebase';
 
 const Home = () => {
-  return (
+  const [, loading] = useAuthState(auth);
+  return loading ? (
+    <Loader />
+  ) : (
     <div className='flex min-h-[100vh]'>
       <SideNav selectedNav='Home' />
       <MainSection />
